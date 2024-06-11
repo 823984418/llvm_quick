5f73640bf0f4b5c7c3a1f2dba83a325b924bbeb9
823984418 <823984418@qq.com>
2024-06-11 17:17:32 +0800

    api

--- src/core/instructions.rs
+++ src/core/instructions.rs
@@ -1,10 +1,11 @@
 use std::ptr::null_mut;
 
 use llvm_sys::core::*;
+use llvm_sys::{LLVMIntPredicate, LLVMOpcode, LLVMRealPredicate};
 
 use crate::core::ValueMetadataEntries;
 use crate::opaque::Opaque;
-use crate::type_tag::{metadata, TypeTag};
+use crate::type_tag::{any, metadata, TypeTag};
 use crate::{BasicBlock, Value};
 
 impl<T: TypeTag> Value<T> {
@@ -37,4 +38,58 @@ impl<T: TypeTag> Value<T> {
     pub fn get_instruction_parent(&self) -> &BasicBlock {
         unsafe { BasicBlock::from_ref(LLVMGetInstructionParent(self.as_raw())) }
     }
+
+    pub fn get_next_instruction(&self) -> Option<&Value<any>> {
+        unsafe { Value::try_from_ref(LLVMGetNextInstruction(self.as_raw())) }
+    }
+
+    pub fn get_previous_instruction(&self) -> Option<&Value<any>> {
+        unsafe { Value::try_from_ref(LLVMGetPreviousInstruction(self.as_raw())) }
+    }
+
+    pub fn instruction_remove_from_parent(&self) {
+        unsafe { LLVMInstructionRemoveFromParent(self.as_raw()) }
+    }
+
+    pub unsafe fn instruction_erase_from_parent(&self) {
+        unsafe { LLVMInstructionEraseFromParent(self.as_raw()) }
+    }
+
+    pub unsafe fn delete_instruction(&self) {
+        unsafe { LLVMDeleteInstruction(self.as_raw()) }
+    }
+
+    pub fn get_instruction_opcode(&self) -> LLVMOpcode {
+        unsafe { LLVMGetInstructionOpcode(self.as_raw()) }
+    }
+
+    pub fn get_i_cmp_predicate(&self) -> LLVMIntPredicate {
+        unsafe { LLVMGetICmpPredicate(self.as_raw()) }
+    }
+
+    pub fn get_f_cmp_predicate(&self) -> LLVMRealPredicate {
+        unsafe { LLVMGetFCmpPredicate(self.as_raw()) }
+    }
+
+    pub fn instruction_clone(&self) -> &Value<T> {
+        unsafe { Value::from_ref(LLVMInstructionClone(self.as_raw())) }
+    }
+
+    pub fn is_a_terminator_inst(&self) -> Option<&Value<T>> {
+        unsafe { Value::try_from_ref(LLVMIsATerminatorInst(self.as_raw())) }
+    }
+
+    pub fn get_num_arg_operands(&self) -> u32 {
+        unsafe { LLVMGetNumArgOperands(self.as_raw()) }
+    }
+
+    pub fn set_instruction_call_conv(&self, cc: u32) {
+        unsafe { LLVMSetInstructionCallConv(self.as_raw(), cc) }
+    }
+
+    pub fn get_instruction_call_conv(&self) -> u32 {
+        unsafe { LLVMGetInstructionCallConv(self.as_raw()) }
+    }
 }
+
+// TODO
