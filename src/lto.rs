7aac6a3a3f24e59f74d7cb64422b6a554c5046c4
823984418 <823984418@qq.com>
2024-06-22 19:22:28 +0800

    api

--- src/lto.rs
+++ src/lto.rs
@@ -1,3 +1,5 @@
+use std::ffi::CStr;
+
 use llvm_sys::lto::*;
 
 use crate::{Opaque, PhantomOpaque};
@@ -36,6 +38,18 @@ pub struct LTOInput {
 
 unsafe impl Opaque for LTOInput {
     type Inner = LLVMOpaqueLTOInput;
+}
+
+pub fn get_version() -> &'static CStr {
+    unsafe { CStr::from_ptr(lto_get_version()) }
+}
+
+pub fn get_error_message() -> &'static CStr {
+    unsafe { CStr::from_ptr(lto_get_error_message()) }
+}
+
+pub fn module_is_object_file(path: &CStr) -> bool {
+    unsafe { lto_module_is_object_file(path.as_ptr()) != 0 }
 }
 
 // TODO
